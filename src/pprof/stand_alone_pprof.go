111846342142708464c29064019c52f122daf2d1
Swaroo <swaroopsrinivasa@gmail.com>
2020-05-17 20:54:07 -0700

    Import error fix

--- src/pprof/stand_alone_pprof.go
+++ src/pprof/stand_alone_pprof.go
@@ -1,9 +1,14 @@
 package main
 import "runtime/pprof"
+import "flag"
+import "os"
+import "runtime"
+import "log"
+import "fmt"
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
-func fibo(n int){
+func fibo(n int) int{
 	i,j := 0,1
 	for k:=2; k<n; k++{
 		i,j = j, i+j 
@@ -26,9 +31,12 @@ func main() {
     }
 
 	fmt.Println("Starting pprof")
-	fibo(5)
-	fibo(50)
-	fibo(100)
+	out := fibo(5)
+	fmt.Println(out)
+	out = fibo(50)
+	fmt.Println(out)
+	out = fibo(100)
+	fmt.Println(out)
 
     if *memprofile != "" {
         f, err := os.Create(*memprofile)
